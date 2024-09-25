8f95d6a89d30a81470ca3c6046db2169cf8b1c89
Thomas <bdachai308@gmail.com>
2024-09-25 13:45:53 +0700

    update

--- src/ascii/mod.rs
+++ src/ascii/mod.rs
@@ -1,40 +1,10 @@
-use core::panic;
+pub const DEFAULT: &[char] = &[
+    ' ', '.', '`', '^', '\"', '\\', ',', ':', ';', 'I', 'l', '!', 'i', '>', '<', '~', '+', '_',
+    '-', '?', ']', '[', '}', '{', '1', ')', '(', '|', '\\', '/', 't', 'f', 'j', 'r', 'x', 'n', 'u',
+    'v', 'c', 'z', 'X', 'Y', 'U', 'J', 'C', 'L', 'Q', '0', 'O', 'Z', 'm', 'w', 'q', 'p', 'd', 'b',
+    'k', 'h', 'a', 'o', '*', '#', 'M', 'W', '&', '8', '%', 'B', '$', '@',
+];
 
-use image::{Pixel, Rgb, Rgba};
-
-pub struct Ascii {
-    color: Rgb<u8>,
-    ch: AsciiChar,
-}
-
-#[repr(transparent)]
-pub struct AsciiChar(pub char);
-
-impl From<u8> for AsciiChar {
-    fn from(value: u8) -> Self {
-        match value {
-            0..10 => AsciiChar(' '),
-            _ => panic!(),
-        }
-    }
-}
-
-impl From<&Rgba<u8>> for Ascii {
-    fn from(value: &Rgba<u8>) -> Self {
-        Self {
-            color: value.to_rgb(),
-            ch: value.0[3].into(),
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_lumi_to_ascii() {
-        let ascii: AsciiChar = 8.into();
-        assert_eq!(ascii.0, ' ')
-    }
+fn u8luma_to_ascii(luma: u8) -> char {
+    DEFAULT[(luma % DEFAULT.len() as u8) as usize]
 }
